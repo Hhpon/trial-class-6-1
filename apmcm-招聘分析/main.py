10b8304a69d86ed9cb83343698bc44dbe1667121
Hhpon <624791164@qq.com>
2021-01-14 23:39:58 +0800

    modify apmcm

--- apmcm-招聘分析/main.py
+++ apmcm-招聘分析/main.py
@@ -5,14 +5,13 @@ from openpyxl import Workbook
 def read_demand(dir, first_level_dir, file):
     workbook = load_workbook('%s%s/%s' % (dir, first_level_dir, file))
     sheet = workbook[workbook.sheetnames[0]]
-    cells = sheet[sheet.dimensions]
+    cells = list(sheet[sheet.dimensions])
     demand_lists = []
-    print(cells.len())
-    cells.pop()
-    # for row in cells:
-    #     for cell in row:
-            # print(cell.value)
-            
+    test_cells = cells[1:3]
+    for row in test_cells:
+        for cell in row:
+            print(cell.value)
+
     print(sheet.dimensions)
 
 
